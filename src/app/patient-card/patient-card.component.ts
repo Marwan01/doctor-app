7c0e1c742e2ddad630bf903c0baa3b3d561d390f
Marwan01 <marwanest@live.fr>
2019-08-28 15:58:46 -0500

    implemented delete method

--- src/app/patient-card/patient-card.component.ts
+++ src/app/patient-card/patient-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Patient } from '../add-patient/Patient';
+import { AngularFirestore } from 'angularfire2/firestore';
+
 
 @Component({
   selector: 'app-patient-card',
@@ -8,8 +10,21 @@ import { Patient } from '../add-patient/Patient';
 })
 export class PatientCardComponent implements OnInit {
   @Input() patient: Patient;
-  constructor() { 
+
+  constructor( private db: AngularFirestore) {
   }
   ngOnInit() {
+  }
+
+  onDelete(patient) {
+    this.db.collection('patients')
+    .get()
+    .subscribe((snapshot) =>{
+      snapshot.forEach(doc => {
+        if(patient.firstname === doc.data().firstname && patient.lastname === doc.data().lastname && patient.diagnosis === doc.data().diagnosis) {
+          this.db.collection('patients').doc(doc.id).delete()
+        }
+      });
+    })
   }
 }
