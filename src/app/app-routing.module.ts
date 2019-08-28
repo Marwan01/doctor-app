c1c5766913fa412b24bea7bece17547f7aaf0c47
Marwan01 <marwanest@live.fr>
2019-08-28 10:11:36 -0500

    configured routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HelpComponent } from './help/help.component';
+import { HomeComponent} from './home/home.component'
 
+const routes: Routes = [
+  { path: 'about', component: AboutComponent},
+  { path: 'help', component: HelpComponent},
+  { path: '', component: HomeComponent}
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [AboutComponent, HelpComponent, HomeComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+
+interface Patient {
+  id: number,
+  firstname: string,
+  lastname: string,
+  diagnosis: string, 
+  examined: boolean,
+  examinedOn?: Date,
+  notes?: string
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  patientsCollection: AngularFirestoreCollection<Patient>
+  patients: Observable<Patient[]>;
+
+  constructor( private db: AngularFirestore) {
+  }
+
+  ngOnInit() {
+    this.patientsCollection = this.db.collection('patients')
+    this.patients = this.patientsCollection.valueChanges()
+  }
+
+}
