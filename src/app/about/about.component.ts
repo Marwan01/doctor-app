e8bd931b3da0c1b7e61c8c1bad5f064d6da1f62b
Marwan01 <marwanest@live.fr>
2019-08-29 15:48:12 -0500

    added about section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  link: String;
   constructor() { }
-
+  link1: String;
+  link2: String;
   ngOnInit() {
+    this.link = '../../assets/images/parrot.gif'
+    this.link1 = '../../assets/images/parrot.gif'
+    this.link2 = '../../assets/images/tunisiaparrot.gif'  
   }
+  ImageClick() {
+    if(this.link === this.link1) {
+      this.link = this.link2;
+    } else {
+      this.link = this.link1;
+    }
+  }
+
 
 }
