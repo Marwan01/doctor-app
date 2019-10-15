81ff7f3f5aa58164a2767f025abb8d2922fab3fd
Marwan01 <marwanest@live.fr>
2019-10-15 09:24:52 -0500

    added end to end test for loading different pages :tram:

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,16 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Doctor App Home', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to doctor-app!');
+  it('should display welcome message in toolbar', () => {
+    page.navigateTo(null);
+    expect(page.getTitleText()).toEqual('Welcome to Doctor App');
   });
 
   afterEach(async () => {
@@ -21,3 +21,46 @@ describe('workspace-project App', () => {
     } as logging.Entry));
   });
 });
+
+describe('Doctor App About', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+    page.navigateTo('/about');
+  });
+
+  it('should navigate to about page', () => {
+    expect(page.getAuthor()).toEqual('Author: Marouen Helali');
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
+
+describe('Doctor App Help', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+    page.navigateTo('/help');
+  });
+
+  it('should navigate to help page', () => {
+    expect(page.getHelpMessage()).toEqual('Step by step tutorial on how to use Doctor-app:');
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
+
